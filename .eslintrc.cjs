1cb2694b0600b793c8b17e311e111258e0339830
stnlyowen <stnlyowen@gmail.com>
2025-02-10 13:11:27 +0700

    12

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -74,4 +74,10 @@ const config = {
   ]
 };
 
-module.exports = config;
+module.exports = {
+  rules: {
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
+  },
+};
