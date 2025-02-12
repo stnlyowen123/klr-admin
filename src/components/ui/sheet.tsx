aa7fed5c50073d66d4811178b85b0b58cfa58e04
stnlyowen <stnlyowen@gmail.com>
2025-02-12 11:05:56 +0700

    qwe

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import * as React from "react"
-import * as SheetPrimitive from "@radix-ui/react-dialog"
+import React from "react"
+import SheetPrimitive from "@radix-ui/react-dialog"
 import { cva, type VariantProps } from "class-variance-authority"
 import { X } from "lucide-react"
 
